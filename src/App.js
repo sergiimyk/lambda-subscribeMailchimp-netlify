5f9e8447e805f1e3f0b642bde981aa7ee81081f7
sergiimyk <sergii.myk@gmail.com>
2021-02-21 21:12:43 +0000

    lambda function test netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    response: '',
+    post: '',
+    name: '',
+    email: '',
+    responseToPost: '',
+  };
+
+  handleMailchimp = async e => {
+    const thankYouMessage = document.querySelector('#thank-you-message');
+    e.preventDefault();
+    const response = await fetch('/.netlify/functions/test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: this.state.name, email: this.state.email }),
+    });
+    const body = await response.text();
+    
+    if (body === "success") {
+      thankYouMessage.classList.add('show');
+    }
+
+    this.setState({ responseToPost: body });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleMailchimp} class="centered mt-10 ml-10 mr-10">
+            <div class="form-group">
+                <label class="form-label" for="firstname">First Name</label>
+                <input class="form-input" type="text" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} id="firstname"/>
+            </div>
+            <div class="form-group">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-input" type="text" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} name="email" id="email"/>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Submit"/>
+            <p id="thank-you-message">
+            Obrigada pela tua subscrição! <strong>Faz o download do teu guia <a href="https://drive.google.com/uc?export=download&id=18q_SSC1y46mJIEQZeeeRaJwTorSij-SO">neste link.</a></strong>            </p>
+{/*             <div class="elementor-message elementor-message-success" role="alert">Obrigada pela tua subscrição! <strong>Faz o download do teu guia <a href="https://www.mariagoncalves.pt/wp-content/uploads/2020/05/Guia-para-Gestão-do-Instagram_Bloglogia.pdf">neste link.</a></strong></div>
+ */}
+        </form>
+        <p>{this.state.responseToPost}</p>
+
+      </div>
+    );
+  }
 }
 
 export default App;
